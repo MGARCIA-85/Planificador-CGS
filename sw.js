5eb78458e4109dbd89b97f8c50df52337020a822
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-09-12 00:20:03 -0600

    V33

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Planificador CGS - Service Worker
 // Sube este número cada vez que publiques cambios para forzar actualización
-const CACHE_VERSION = 'v30';
+const CACHE_VERSION = 'v33';
 const CACHE_NAME = 'planificador-cgs-' + CACHE_VERSION;
 
 const ASSETS = [
